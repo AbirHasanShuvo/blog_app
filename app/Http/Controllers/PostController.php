23d56b245eb5643dddd012b0bf04446d7c9082d4
Md. Abir Hasan <62834401+AbirHasanShuvo@users.noreply.github.com>
2026-02-17 15:37:37 +0600

    done with the post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,19 +2,94 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    protected $fillable = [
-        'user_id',
-        'category_id',
-        'title',
-        'slug',
-        'content',
-        'excerpt',
-        'thumbnail',
-        'status',
-        'published_at'
-    ];
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), rules : [
+            'user_id' => 'required|numeric',
+            'category_id' => 'required|numeric',
+            'title' => 'required',
+            'content' => 'required',
+            'thumbnail' => 'nullable|image|max:2048'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'Failed',
+                'message' => $validator->errors()
+            ]);
+        }
+
+        //check it is logged in user or not because if it is not then anyone can give anyone id to post 
+
+        $loggedInUser = Auth::user();
+
+        if($loggedInUser->id != $request->user_id){
+            return response()->json([
+                'status' => 'Failed',
+                'message' => 'Un-authorised access'
+            ], 403);
+        }
+
+        $category = Category::find($request->category_id);
+
+        if(!$category){
+            return response()->json([
+                'status' => 'Failed',
+                'message' => 'No category found'
+            ]);
+        }
+
+        //uploading image 
+
+        if($request->hasFile('thumbnail')){
+            $file = $request->
+        }
+
+
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'excerpt',
+        'thumbnail',
+        'status',
+        'published_at'
+    ];
 }
